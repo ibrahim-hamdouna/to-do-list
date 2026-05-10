82f5ada3586e7b9813fd9d0830fdda3fffc15918
Ibrahim Tawfeeq Hamdouna <ibrahimhamdouna11@gmail.com>
2026-05-10 17:35:39 +0300

    Fix NameError by importing os module

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,8 +74,6 @@ WSGI_APPLICATION = 'to_do_list.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-import os
 
 DATABASES = {
     'default': {
